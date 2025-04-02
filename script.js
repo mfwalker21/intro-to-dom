d74c0bb77c362f2603c551d21e14d3a90a84de32
Mya <myawalkerinfo@gmail.com>
2025-04-02 01:21:12 +0000

    first examples

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const title = document.getElementById("title");
+const pTag = document.querySelector("p");
+const header4 = document.querySelector("h4");
+const header6 = document.querySelector("h6");
+const paragraph = document.querySelectorAll(".description");
+
+    console.log(title);
+    console.log(pTag);
+    console.log(header4);
+    console.log(header6); 
+    console.log(paragraph);
+
+})
